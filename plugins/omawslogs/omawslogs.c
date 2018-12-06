d48402fca438baf2ae5afbce8fe3b62b08d2dd1d
Martin Schütte <info@mschuette.name>
2018-12-06 16:05:48 +0100

    plugins/omawslogs: refactor to use CloudWatchLogsController

--- plugins/omawslogs/omawslogs.c
+++ plugins/omawslogs/omawslogs.c
@@ -22,17 +22,6 @@
  */
 #include "config.h"
 #include "rsyslog.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <unistd.h>
-#include <errno.h>
-#include <netdb.h>
-#include <time.h>
-#include <sys/socket.h>
-#include <dlfcn.h>
 #include "conf.h"
 #include "syslogd-types.h"
 #include "srUtils.h"
@@ -56,10 +45,7 @@ DEFobjCurrIf(glbl)
 DEFobjCurrIf(datetime)
 
 typedef struct _instanceData {
-    char *logGroup;
-    char *logStream;
-    char *awsRegion;
-    struct awslib_instance *awslib_inst;
+    CloudWatchLogsController *ctl;
 } instanceData;
 
 typedef struct wrkrInstanceData {
@@ -67,9 +53,10 @@ typedef struct wrkrInstanceData {
 } wrkrInstanceData_t;
 
 typedef struct configSettings_s {
-    char *logGroup;
-    char *logStream;
-    char *awsRegion;
+    char *log_group;
+    char *log_stream;
+    char *aws_region;
+    // TODO: add template?
 } configSettings_t;
 static configSettings_t cs;
 
@@ -88,16 +75,13 @@ static struct cnfparamblk actpblk =
 	};
 
 
-
 BEGINinitConfVars		/* (re)set config variables to default values */
 CODESTARTinitConfVars
-	cs.logGroup = NULL;
-	cs.logStream = NULL;
-	cs.awsRegion = NULL;
+	cs.log_group = NULL;
+	cs.log_stream = NULL;
+	cs.aws_region = NULL;
 ENDinitConfVars
 
-/* forward definitions (as few as possible) */
-// TODO
 
 BEGINcreateInstance
 CODESTARTcreateInstance
@@ -127,10 +111,9 @@ ENDfreeWrkrInstance
 BEGINdbgPrintInstInfo
 CODESTARTdbgPrintInstInfo
     dbgprintf("awslogs");
-    dbgprintf("\tregion='%s'\n", pData->awsRegion);
-    dbgprintf("\tgroup='%s'\n",  pData->logGroup);
-    dbgprintf("\tstream='%s'\n", pData->logStream);
+    dbgprintf("\tcontroller='%p'\n", pData->ctl);
 ENDdbgPrintInstInfo
+
 
 BEGINtryResume
     CODESTARTtryResume
@@ -141,15 +124,16 @@ ENDtryResume
 
 BEGINdoAction
 	const instanceData *const __restrict__ pData = pWrkrData->pData;
+	CloudWatchLogsController *ctl = pData->ctl;
 	char* toWrite;
 	uint len;
 CODESTARTdoAction
 	toWrite = (char*) ppString[0];
 	len = strlen(toWrite);
 
-    DBGPRINTF("omawslogs doAction([%d] %s, %s, %s, %s)\n",
-			  len, toWrite, pData->awsRegion, pData->logGroup, pData->logStream);
-    iRet = aws_logs_msg_put(pData->awslib_inst, pData->logGroup, pData->logStream, (const char *) ppString[0]);
+    DBGPRINTF("omawslogs doAction([%d] %s, %p)\n",
+			  len, toWrite, ctl);
+    iRet = aws_logs_msg_put(ctl, (const char *) ppString[0]);
 
 	if(iRet == RS_RET_OK) {
         DBGPRINTF("sent something\n");
@@ -162,6 +146,9 @@ ENDdoAction
 BEGINnewActInst
 	struct cnfparamvals *pvals;
 	int rc;
+	char *region = NULL;
+	char *group = NULL;
+	char *stream = NULL;
 CODESTARTnewActInst
 	if((pvals = nvlstGetParams(lst, &actpblk, NULL)) == NULL) {
 		ABORT_FINALIZE(RS_RET_MISSING_CNFPARAMS);
@@ -173,42 +160,36 @@ CODESTARTnewActInst
         if(!pvals[i].bUsed)
             continue;
         if(!strcmp(actpblk.descr[i].name, "region")) {
-            pData->awsRegion = es_str2cstr(pvals[i].val.d.estr, NULL);
+            region = es_str2cstr(pvals[i].val.d.estr, NULL);
         } else if(!strcmp(actpblk.descr[i].name, "group")) {
-            pData->logGroup  = es_str2cstr(pvals[i].val.d.estr, NULL);
+            group  = es_str2cstr(pvals[i].val.d.estr, NULL);
         } else if(!strcmp(actpblk.descr[i].name, "stream")) {
-            pData->logStream = es_str2cstr(pvals[i].val.d.estr, NULL);
+            stream = es_str2cstr(pvals[i].val.d.estr, NULL);
         } else {
             DBGPRINTF("omawslogs: program error, non-handled "
                       "param '%s'\n", actpblk.descr[i].name);
         }
     }
 
-    /* like setInstParamDefaults, but only if no user params present */
-	if (!pData->logGroup) {
-		pData->logGroup = "rsyslog";
-	}
-    if (!pData->logStream) {
-        if((pData->logStream = calloc(1, HOST_NAME_MAX)) == NULL) {
-            return RS_RET_OUT_OF_MEMORY;
-        }
-        gethostname(pData->logStream, HOST_NAME_MAX);
-    }
-
     CODE_STD_STRING_REQUESTnewActInst(1)
 	CHKiRet(OMSRsetEntry(*ppOMSR, 0, (uchar*)strdup("RSYSLOG_FileFormat"),
 						 OMSR_NO_RQD_TPL_OPTS));
 
-    pData->awslib_inst = aws_init(pData->awsRegion);
-	rc = aws_logs_ensure(pData->awslib_inst, pData->logGroup, pData->logStream);
+    pData->ctl = aws_init(region, group, stream);
+    rc = aws_logs_ensure(pData->ctl);
+
 	if (rc) {
         DBGPRINTF("omawslogs: program error, aws_logs_ensure returned %d with msg '%s'\n",
-                rc, pData->awslib_inst->last_error_message);
+                rc, aws_logs_get_last_error(pData->ctl));
         ABORT_FINALIZE(RS_RET_DATAFAIL);
 	} else {
         DBGPRINTF("omawslogs: aws_logs_ensure successful\n");
 	}
 
+	// TODO: can we avoid the es_str2cstr() and free() for params?
+    free(region);
+    free(group);
+    free(stream);
 CODE_STD_FINALIZERnewActInst
 	cnfparamvalsDestruct(pvals, &actpblk);
 ENDnewActInst
@@ -222,9 +203,12 @@ static rsRetVal freeConfigVariables(void)
 {
 	DEFiRet;
 
-    cs.awsRegion = NULL;
-    cs.logGroup = NULL;
-    cs.logStream = NULL;
+    free(cs.aws_region);
+    cs.aws_region = NULL;
+    free(cs.log_group);
+    cs.log_group = NULL;
+    free(cs.log_stream);
+    cs.log_stream = NULL;
 
 	RETiRet;
 }
@@ -271,8 +255,8 @@ CODEmodInit_QueryRegCFSLineHdlr
 
 	DBGPRINTF("omawslogs version %s initializing\n", VERSION);
 
-	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "actionawsloggroup",    0, eCmdHdlrGetWord, NULL, &cs.logGroup,   STD_LOADABLE_MODULE_ID));
-	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "actionawslogstream",   0, eCmdHdlrGetWord, NULL, &cs.logStream,  STD_LOADABLE_MODULE_ID));
-	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "actionawsregion",      0, eCmdHdlrGetWord, NULL, &cs.awsRegion, STD_LOADABLE_MODULE_ID));
+	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "actionawsloggroup",    0, eCmdHdlrGetWord, NULL, &cs.log_group,   STD_LOADABLE_MODULE_ID));
+	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "actionawslogstream",   0, eCmdHdlrGetWord, NULL, &cs.log_stream,  STD_LOADABLE_MODULE_ID));
+	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "actionawsregion",      0, eCmdHdlrGetWord, NULL, &cs.aws_region, STD_LOADABLE_MODULE_ID));
 	CHKiRet(omsdRegCFSLineHdlr(	(uchar*) "resetconfigvariables", 1, eCmdHdlrCustomHandler, resetConfigVariables, NULL, STD_LOADABLE_MODULE_ID));
 ENDmodInit
